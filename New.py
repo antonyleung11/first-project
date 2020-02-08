a91d7bcf01526c9c0a40720067825c0e6640edfb
Antony Leung <antonyleung11@gmail.com>
2020-02-08 11:08:27 +1100

    first commit

---
+++ New.py
@@ -0,0 +1,15 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+# print(sys.executable)
+r = requests.get("http://www.ozbargain.com.au")
+print(r.status_code)
+print(r.ok)
+
+print(r.ok)
+print(r.ok)
+print(r.ok)
